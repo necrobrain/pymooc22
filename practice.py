be7e3701b5f7bda94cc73ceae60b0af382801eae
Fen <fen.astrand@gmail.com>
2022-03-10 03:37:29 +0200

    moi

--- practice.py
+++ practice.py
@@ -1,3 +1,12 @@
 print("Welcome!")
 
-name = input("Give me a name.")
+while True: 
+    name = input("Give me a name. ")
+
+    if name == "Fen":
+        print("Idiootti!")
+
+    if name != "Fen":
+        break
+
+print("Et ole idiootti.")
